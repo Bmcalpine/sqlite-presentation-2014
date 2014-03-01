313d89d1143e6a4bf7367adf05964aba2ebd4be7
Jamie Phillips <jphillips@gisinc.com>
2014-03-01 08:19:02 -0500

    Updated testing file for creation

--- create/create-file.ps1
+++ create/create-file.ps1
@@ -1,6 +1,8 @@
 $i = 1
 do {
-    python -m cProfile -o testing-stats create-gdb.py -n -t -p
+    python -m cProfile -o file-stats$i create-gdb.py -n file-gdb$i -t FILGDB -p "C:\\temp\\databases"
+    python -m cProfile -o sqlite-stats$i create-gdb.py -n sqlite-gdb$i -t ST_GEOMETRY -p "C:\\temp\\databases"
+    python -m cProfile -o spatial-stats$i create-gdb.py -n spatialite$i -t SPATIALITE -p "C:\\temp\\databases"
     $i++
 }
 until($i -gt 10)
