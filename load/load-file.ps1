b23f3a7a4d44996d739008bd31d6abbc4ca43dfc
Jamie Phillips <jphillips@gisinc.com>
2014-03-01 09:12:29 -0500

    Adding data and creating loading powerhsell script

--- load/load-file.ps1
+++ load/load-file.ps1
@@ -1,6 +1,10 @@
 $i = 1
+$shapefile = "C:\\temp\\testing-data\\streets\\street_segmet.shp"
+
 do {
-    python -m cProfile -o testing-stats load-data.py -f -d
+    python -m cProfile -o loading-file-stats$1 load-data.py -f $shapefile -d "C:\\temp\\databases\file-gdb$i"
+    python -m cProfile -o loading-sqlite-stats$1 load-data.py -f $shapefile -d "C:\\temp\\databases\sqlite-gdb$i"
+    python -m cProfile -o loading-spatial-stats$1 load-data.py -f $shapefile -d "C:\\temp\\databases\spatial-stats$i"
     $i++
 }
 until($1 -gt 10)
