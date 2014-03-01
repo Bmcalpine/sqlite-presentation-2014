d5f6caa36d6fe02365362834f0ecc9f4c6e365c9
Jamie Phillips <jphillips@gisinc.com>
2014-03-01 08:39:05 -0500

    Updated typo in creation script

--- create/create.ps1
+++ create/create.ps1
@@ -1,6 +1,6 @@
 $i = 1
 do {
-    python -m cProfile -o file-stats$i create-gdb.py -n file-gdb$i -t FILGDB -p "C:\\temp\\databases"
+    python -m cProfile -o file-stats$i create-gdb.py -n file-gdb$i -t FILEGDB -p "C:\\temp\\databases"
     python -m cProfile -o sqlite-stats$i create-gdb.py -n sqlite-gdb$i -t ST_GEOMETRY -p "C:\\temp\\databases"
     python -m cProfile -o spatial-stats$i create-gdb.py -n spatialite$i -t SPATIALITE -p "C:\\temp\\databases"
     $i++
